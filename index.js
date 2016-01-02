41fae371d650003162a425a9a63c40f64e9ae2ec
Mike Dunn <michael.dunn@rewardstyle.com>
2016-01-01 20:21:46 -0600

    Moving more logic server side

--- index.js
+++ index.js
@@ -1,42 +1,103 @@
 var chatLog = ['archive cleared'];
+var fs = require('fs');
 var express = require('express');
 var app = express();
 var port = 3700;
-app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
+
+
+// Application settings
+app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/tpl');
 app.set('view engine', 'jade');
 app.engine('jade', require('jade').__express);
 app.get("/", function(req, res){
 
-    res.render('page',  {
-        pageData: {
-            'chatLog': chatLog
-        }
-    });
+    res.render('page');
 
 })
 
 
+// Message styles
+var chatStyle = 'color: #800080; font-size:15px; font-family: helvetica;';
+var archStyle = 'color: #a694a6; font-size:15px; font-family: helvetica;';
+var sysStyle = 'color: #ccc; font-size:12px; font-family: helvetica;';
+
+
 io.sockets.on('connection', function (socket) {
 
-    // New connection messaging
+    // Log connected user
     console.log(
-        '\x1b[31m',
-        socket.handshake.address + ' connected' ,
+        '\x1b[32m',
+        socket.handshake.address + ' connected -- ' +
+        io.engine.clientsCount + ' total connections' ,
         '\x1b[0m'
     );
-
+    // Send app instructions
     socket.emit('message', {
 
-        message: 'new user connected',
-        type: 'system'
+        message: '-- use msg(string) to chat',
+        style: sysStyle
 
     });
+    // Send archive messages
+    for(var i = 0; i<chatLog.length; i++) {
+
+        socket.emit('message', {
+
+            message: chatLog[i],
+            style: archStyle
+
+        });
+
+    };
+    // Send new user status to all connections
+    io.sockets.emit('message', {
+
+        message: 'new user connected',
+        style: sysStyle
+
+    });
+
 
     // Event handler
     socket.on('send', function (data) {
 
+        // Rules for omitting messages
+        // Character limit
+        if ( data.message.length > 150 ) {
+
+            socket.emit('message', {
+
+                message: 'messages may be a maximum of 150 characters',
+                style: sysStyle
+
+            });
+            console.log(
+                '\x1b[31m',
+                '150 Limit --- ' + socket.handshake.address + ' ' + data.message.length + ' characters' ,
+                '\x1b[0m'
+            );
+            return;
+
+        }
+        // Banned
+        /*
+        if (banned) {
+
+            return;
+
+        }
+        if (throttled) {
+
+            return;
+
+        }
+        */
+
+        data.style = chatStyle;
+
+        // Emit to client handler
         io.sockets.emit('message', data);
 
         // Save last 5 messages for archive
@@ -45,7 +106,13 @@ io.sockets.on('connection', function (socket) {
             chatLog.shift();
         }
 
-        // Log chat
+        // Log messages
+        fs.appendFile("msgLog.txt",
+            data.message +
+            ' || ' + Date() +
+            ' || ' + socket.handshake.address +
+            '\n'
+        );
         console.log(
             data.message,
             '\x1b[36m',
--- public/chat.js
+++ public/chat.js
@@ -2,34 +2,30 @@ window.onload = function () {
 
     // Chat Commands
     msg = function (text) {
+
+        // Send message to server
         socket.emit('send', { message: text });
+
     };
 
 
-    // Message styles
-    var chatStyle = 'color: #800080; font-size:15px; font-family: helvetica;';
-    var archStyle = 'color: #a694a6; font-size:15px; font-family: helvetica;';
-    var sysStyle = 'color: #ccc; font-size:12px; font-family: helvetica;';
-
-
-    // Message handler
-    var socket = io.connect('http://54.200.115.24:3700');
-    //var socket = io.connect('http://localhost:3700');
+    // Handle receiving messages
+    // var socket = io.connect('http://54.200.115.24:80');
+    var socket = io.connect('http://localhost:3700');
     socket.on('message', function (data) {
 
         if (data.message) {
 
-            var style = (data.type === 'system') ? sysStyle : chatStyle;
             addMessage({
                 'message': data.message,
-                'type': style
+                'style': data.style
             });
 
         }
 
     });
 
-
+    // Console painting system
     var msgQueue = [];
     addMessage = function (msg) {
 
@@ -39,26 +35,10 @@ window.onload = function () {
         // Output messages in queue
         for (var i = 0; i < msgQueue.length; i++) {
 
-            console.log('%c' + msgQueue[i].message, msgQueue[i].type)
+            console.log('%c' + msgQueue[i].message, msgQueue[i].style)
 
         }
 
     }
 
-
-    // Initial messages
-    // Add instruction messages
-    addMessage({
-        'message': '-- use msg(string) to chat',
-        'type': sysStyle
-    });
-    // Add archive messages
-    for(var i = 0; i<chatLog.length; i++) {
-
-        addMessage({
-            'message': chatLog[i],
-            'type': archStyle
-        })
-
-    }
 }
